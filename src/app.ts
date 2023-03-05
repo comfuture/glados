228540f476d250cbba7ab2979357371e00d2eaa5
comfuture <comfuture@iplateia.com>
2023-03-06 01:15:01 +0900

    provide app token

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ dotenv.config();
 export const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true,
 });
 
