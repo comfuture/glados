279863c043ab307c200673c7f63e8a0a4e07f6b9
comfuture <comfuture@iplateia.com>
2023-03-08 00:13:44 +0900

    slashcommand test

--- src/glados/index.ts
+++ src/glados/index.ts
@@ -1,3 +1,4 @@
+import * as shellQuote from "shell-quote";
 import { App, AppOptions } from "@slack/bolt";
 import { GladosPlugin } from "./types";
 
@@ -7,30 +8,35 @@ export function definePlugin(plugin: GladosPlugin) {
 }
 
 const corePlugin = definePlugin({
-  name: "glados",
+  name: "GLaDOS",
   description: "GLaDOS core plugin",
   setup(app) {
-    app.command("/glados", async ({ command, ack, say, client }) => {
+    app.command("/glados", async ({ command: _command, ack, say, client }) => {
       await ack();
-      await say(command.text);
-      await say(JSON.stringify(command.text.split(" ")));
+      const [command, text] = _command.text.split(" ", 2);
+      const args = shellQuote.parse(text ?? "");
+      await say(`you called ${command} with ${JSON.stringify(args)}`);
     });
   },
 });
 
 export class Bot extends App {
-  private plugins: GladosPlugin[] = [];
+  private _plugins: GladosPlugin[] = [];
 
   constructor(options: AppOptions) {
     super(options);
     this.registerPlugin(corePlugin);
   }
 
+  get plugins() {
+    return this._plugins;
+  }
+
   /** plugin을 등록합니다. */
   public registerPlugin(plugin: GladosPlugin) {
     try {
       plugin.setup(this);
-      this.plugins.push(plugin);
+      this._plugins.push(plugin);
       console.info(`[GLaDOS] ${plugin.name} plugin is loaded.`);
     } catch (e) {
       console.error(`[GLaDOS] ${plugin.name} plugin is failed to load.`);
